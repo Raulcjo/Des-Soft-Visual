6dafc6c471f6756728acaf122a9ac06ee91eed0b
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-08-07 21:31:04 -0300

    Aula 2

---
+++ Aula2/Banco/Banco/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente c1 = new ContaCorrente();
+            c1.titular = "012345678900";
+            c1.saldo = 100.00;
+
+            c1.Depositar(-1);
+            Console.WriteLine("Saldo de " + c1.titular + " é " + c1.saldo);
+            c1.Depositar(200);
+            Console.WriteLine("Saldo de " + c1.titular + " é " + c1.saldo);
+            c1.Sacar(350);
+            Console.WriteLine("Saldo de " + c1.titular + " é " + c1.saldo);
+            c1.Sacar(50);
+            Console.WriteLine("Saldo de " + c1.titular + " é " + c1.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
