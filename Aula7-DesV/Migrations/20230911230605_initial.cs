16e3b0142d8dc23e099e3a8f2d5e5232e333a981
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-09-11 21:47:20 -0300

    Aula 7 - 11/09/2023

---
+++ Aula7-DesV/Migrations/20230911230605_initial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace VendasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: true),
+                    Email = table.Column<string>(type: "longtext", nullable: false),
+                    Telefone = table.Column<string>(type: "longtext", nullable: true),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ Aula7-DesV/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using MySql.EntityFrameworkCore.Extensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace VendasAPI.Models
+{
+    public class Produto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        [Required(ErrorMessage = "Código de barras é obrigatório")]
+        public int codigoBarras { get; set; }
+        [Required(ErrorMessage="Preço é obrigatório")]
+        public double? preco { get; set; }  
+        public int estoque {  get; set; }
+
+        
+
+    }
+}
