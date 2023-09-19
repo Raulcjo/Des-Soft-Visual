9535b64087c58acc3661f987636c27e9b52df01d
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-09-18 21:59:41 -0300

    Aula 8 - 18/09/2023

---
+++ Aula8-DesSV/Data/ExemploContext.cs
@@ -0,0 +1,14 @@
+﻿using Aula8_DesSV.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aula8_DesSV.Data
+{
+    public class ExemploContext : DbContext
+    {
+        //Construtor
+        public ExemploContext(DbContextOptions<ExemploContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
