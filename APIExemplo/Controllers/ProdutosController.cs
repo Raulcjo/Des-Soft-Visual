279a8fa799f58c9d4fda9a2cd25ba81f88703666
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-09-25 20:31:15 -0300

    Add files via upload

---
+++ APIExemplo/Controllers/ProdutosController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIExemplo.Data;
+using APIExemplo.Models;
+using System.Diagnostics.Tracing;
+
+namespace APIExemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ExemploContext _context;
+
+        public ProdutosController(ExemploContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Produtos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+          if (_context.Produtos == null)
+          {
+              return NotFound();
+          }
+            return await _context.Produtos.ToListAsync();
+        }
+
+        // GET: api/Produtos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+          if (_context.Produtos == null)
+          {
+              return NotFound();
+          }
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return produto;
+        }
+
+        // PUT: api/Produtos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduto(int id, Produto produto)
+        {
+            if (id != produto.IdProduto)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProdutoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Produtos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+          if (_context.Produtos == null)
+          {
+              return Problem("Entity set 'ExemploContext.Produtos'  is null.");
+          }
+  
+            double preco = produto.Preco;
+
+            if(preco < 0.01)
+            {
+                return Problem("Preço é menor que 0,01!");
+            }
+            else
+            {
+                _context.Produtos.Add(produto);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetProduto", new { id = produto.IdProduto }, produto);
+            }
+            
+        }
+
+        private ActionResult<Produto> NonActionAttribute()
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE: api/Produtos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduto(int id)
+        {
+            if (_context.Produtos == null)
+            {
+                return NotFound();
+            }
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProdutoExists(int id)
+        {
+            return (_context.Produtos?.Any(e => e.IdProduto == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ APIExemplo/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIExemplo.Models
+{
+    public class Produto
+    {
+        [Key]
+        [Required (ErrorMessage="Id deve ser preenchido")]
+        public int IdProduto { get; set; }
+        public string Descricao { get; set; }
+        [Required (ErrorMessage="Preço deve ser preenchido")]
+        public double Preco { get; set; }
+        public int QtnMin { get; set; }
+        public string Categoria { get; set; }
+        public double Tamanho { get; set;}
+        public string Cor { get; set; }
+    }
+}
